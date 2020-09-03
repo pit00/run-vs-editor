31d7691f801530d212716ea655f705bcc26fe3e8
zvikarp <zvi@zektec.com>
2020-09-03 23:08:22 +0300

    🚧 added template with actionable items in editor

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,40 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import {
+  ExtensionContext,
+  languages,
+  commands,
+  Disposable,
+  workspace,
+  window,
+} from "vscode";
+import { CodelensProvider } from "./CodelensProvider";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+let disposables: Disposable[] = [];
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "commaned-runner" is now active!');
+export function activate(context: ExtensionContext) {
+  const codelensProvider = new CodelensProvider();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('commaned-runner.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+  languages.registerCodeLensProvider("*", codelensProvider);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Commaned Runner!');
-	});
+  commands.registerCommand("commaned-runner.enableCodeLens", () => {
+    workspace
+      .getConfiguration("commaned-runner")
+      .update("enableCodeLens", true, true);
+  });
 
-	context.subscriptions.push(disposable);
+  commands.registerCommand("commaned-runner.disableCodeLens", () => {
+    workspace
+      .getConfiguration("commaned-runner")
+      .update("enableCodeLens", false, true);
+  });
+
+  commands.registerCommand("commaned-runner.codelensAction", (args: any) => {
+    window.showInformationMessage(`CodeLens action clicked with args=${args}`);
+  });
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+  if (disposables) {
+    disposables.forEach((item) => item.dispose());
+  }
+  disposables = [];
+}
